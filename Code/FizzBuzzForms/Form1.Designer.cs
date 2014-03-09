a527d8b6db2ff6e9f76b03d405bdf7b0687457d5
ficek.rafal <ficek.rafal@gmail.com>
2014-03-09 18:19:15 +0100

    Dodanie solucji

---
+++ Code/FizzBuzzForms/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FizzBuzzForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMain = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbMain
+            // 
+            this.lbMain.FormattingEnabled = true;
+            this.lbMain.Location = new System.Drawing.Point(90, 59);
+            this.lbMain.Name = "lbMain";
+            this.lbMain.Size = new System.Drawing.Size(148, 82);
+            this.lbMain.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lbMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMain;
+
+    }
+}
+
---
+++ Code/FizzBuzzTEST/ProgramTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FizzBuzz;
+
+namespace FizzBuzzTEST
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void WypisujeJedenNaJeden()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(1);
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void WypisujeFizzNaTrzy()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(3);
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void WypisujeBuzzNaPiec()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(5);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void WypisujeFizzBuzzNaPietnascie()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(15);
+            Assert.Equal("FizzBuzz", result);
+        }
+
+        [Fact]
+        public void TestOd1Do5()
+        {
+            var game = new FizzBuzzGame();
+            var expected = new string[] { "1", "2", "Fizz", "4", "Buzz" };
+            var result = game.Range().Take(5);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestOd1Do3()
+        {
+            var game = new FizzBuzzGame();
+            var expected = new string[] { "1", "2", "Fizz" };
+            var result = game.Range(3);
+            Assert.Equal(expected, result);
+        }
+    }
+}
