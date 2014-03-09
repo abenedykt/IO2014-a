e62b31446813838211df13fee77d3883b1ba1d42
Krzysztof Bizoń <krzys19914@o2.pl>
2014-03-09 21:53:09 +0100

    Add FizzBuzz code

---
+++ Code/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+            var game = new FizzBuzzGame();
+            var result = game.Range(count);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            File.WriteAllLines("result.txt", result.ToArray());
+
+            Console.ReadKey();
+        }
+    }
+}
