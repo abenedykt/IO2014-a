73a19ccfbff73f85d69b63961ea92508de1afe0c
pawelduda <ciakoc123@gmail.com>
2014-03-10 16:21:35 +0100

    Zad2Dzialki

---
+++ Zad2Dzialki/Zad2Dzialki/GeoRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Zad2Dzialki
+{
+    class GeoRecord
+    {
+        public string nrDzialki;
+        public string x;
+        public string y;
+        public string minX;
+        public string maxX;
+        public string minY;
+        public string maxY;
+
+        public GeoRecord(IGeoFileReader reader, IGeoRepository repository)
+        {
+            
+        }
+    }
+}
---
+++ Zad2Dzialki/Zad2Dzialki/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace Zad2Dzialki
+{
+    class Test
+    {
+        public string ExampleRecord = 
+        @"               1.1-800/128
+                5544943.71        6532099.62         5544943.71        6532099.62 N  7
+        7
+                      1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7
+                      1-7880        5544908.34        6532098.97        5544908.34        6532098.97   0 N N  7
+                      1-7807        5544907.94        6532075.96        5544907.94        6532075.96   0 N N  7
+                      1-7856        5544928.59        6532085.45        5544928.59        6532085.45   0 N N  7
+                      1-7881        5544975.70        6532098.40        5544975.70        6532098.40   0 N N  7
+                      1-7907        5544972.66        6532107.54        5544972.66        6532107.54   0 N N  7
+                      1-7923        5544969.02        6532117.26        5544969.02        6532117.26   0 N N  7
+        ";
+
+        [Fact]
+        public void arrange()
+        {
+            var fileReader = Substitute.For<IGeoFileReader>();
+            var repository = Substitute.For<IGeoRepository>();
+            var geoImport = new GeoRecord(fileReader, repository);
+
+            fileReader.Returns();
+        }
+    }
+}
