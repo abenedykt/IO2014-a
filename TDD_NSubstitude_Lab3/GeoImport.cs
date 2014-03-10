c8904b1e39184be1eca330bd42eb1488693d39dd
Adam Kawik <akawik@gmail.com>
2014-03-10 16:22:06 +0100

    Labolatorium 3 - TDD NSubstitute - Zadanie GEO ( nie kompletne )

---
+++ TDD_NSubstitude_Lab3/GeoImport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_NSubstitude_Lab3
+{
+    class GeoImport
+    {
+        private IGeoFileReader reader;
+        private IGeoRepository repository;
+
+        public GeoImport(IGeoFileReader fileReader, IGeoRepository repo)
+        {
+            this.reader = fileReader;
+            this.repository = repo;
+        }
+    }
+}
