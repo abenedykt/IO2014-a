76b4386c03c09c2d6ac17afd4125a2baa1552572
pwlgbb <pgraczek@gmail.com>
2014-03-10 20:53:25 +0100

    lab3
    Signed-off-by: pwlgbb <pgraczek@gmail.com>

---
+++ Io/NSubstitute_Projekt/Geo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSubstitute_Projekt
+{
+
+    interface IGeoRepository
+    {
+        void Add(string naDzialki, double x, double y, double minX, double minY, double maxX, double maxY);
+    }
+
+    interface IGeoFileReader
+    {
+        IEnumerable<string> GetRecords();
+    }
+
+    class GeoInport
+    {
+        private IGeoFileReader fileReader;
+        private IGeoRepository repository;
+
+        public GeoInport(IGeoFileReader fileReader, IGeoRepository repository)
+        {
+            this.fileReader = fileReader;
+            this.repository = repository;
+        }
+    }    
+}
