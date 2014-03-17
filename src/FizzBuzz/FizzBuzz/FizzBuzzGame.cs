b06c12d0695f46fe44a4493f9e70c2d0e20b607e
Dariusz Myszka <ddmyszka@gmail.com>
2014-03-17 15:27:19 +0100

    Dodano FizzBuzz

---
+++ src/FizzBuzz/FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGame
+    {
+        public string Play(int number)
+        {
+            if (number % 15 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
+        }
+
+        public IEnumerable<string> Range()
+        {
+            var i = 1;
+            while (true)
+                yield return Play(i++);
+        }
+
+        public IEnumerable<string> Range(int count)
+        {
+            return Range().Take(count).ToArray();
+        }
+    }
+}
---
+++ src/FizzBuzz/FizzBuzzForms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzzForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddItems();
+        }
+
+        private void AddItems()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Range(10);
+        }
+    }
+}
