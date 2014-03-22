a6742d63a8d28f1314169405581bc44e4c33f085
ficek.rafal <ficek.rafal@gmail.com>
2014-03-22 21:43:44 +0100

    Dodanie zadania z Angularem

---
+++ html/AngularTest/Scripts/app/main.js
@@ -0,0 +1,8 @@
+﻿function mainController($scope) {
+    var fizzBuzz = ["1", "2", "Fizz", "4", "Buzz", "6", "7", "8"];
+	$scope.input = 1;
+
+	$scope.output = function(index) {
+		return fizzBuzz.slice(0, index);
+	};
+}
