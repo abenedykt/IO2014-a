65b809ac999c230f9dcfcadab192e0dece691a4a
pawelduda <ciakoc123@gmail.com>
2014-03-24 16:28:15 +0100

    Zad 3 Angular

---
+++ Zad3AngularPlusMVC/AngularPlusMVC/AngularPlusMVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace AngularPlusMVC.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        //
+        // GET: /FizzBuzz/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult GetResult(int id)
+        {
+            var result = Enumerable.Range(0, id).ToArray();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string json = js.Serialize(result);
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
