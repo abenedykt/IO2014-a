bad92d9501572bd267759f3487539546c3a6594d
pawelduda <ciakoc123@gmail.com>
2014-03-31 16:22:15 +0200

    Zad 4 Dependency Injection

---
+++ Zad4DependencyInjection/MVCDependency/MVCDependency/Controllers/TextController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abstract;
+
+namespace MVCDependency.Controllers
+{
+    public class TextController : Controller
+    {
+        private IEvent Event;
+        private IRepository Repository;
+        private IApplication Application;
+
+        public TextController(IEvent Event, IRepository Repository, IApplication Application)
+        {
+            this.Event = Event;
+            this.Repository = Repository;
+            this.Application = Application;
+        }
+
+        //
+        // GET: /Text
+
+        public ActionResult Index()
+        {
+            Repository.GetAll();
+
+            return View();
+        }
+
+    }
+}
---
+++ Zad4DependencyInjection/MVCDependency/Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Abstract;
+
+namespace Repository
+{
+    public class Repository : IRepository
+    {
+        private IEnumerable<IEvent> Events;
+ 
+        public Repository(IApplication Application)
+        {
+            
+        }
+
+        public IEnumerable<string> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
