946776d4c210a2e15d62fd0145b2c0e4f8ffd8f3
Krzysztof Bizoń <krzys19914@o2.pl>
2014-04-07 15:54:03 +0200

    LSP

---
+++ Code/LSP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class Kwadrat : Prostokat
+    {
+        public Kwadrat(double a)
+            : base(a, a)
+        {
+        }
+    }
+
+    public class Prostokat
+    {
+        private double a;
+        private double b;
+
+        public Prostokat(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double Obwod()
+        {
+            return 2 * a + 2 * b;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prostokat = new Prostokat(3, 4);
+            Console.WriteLine(prostokat.Obwod());
+
+            var kwadrat = new Kwadrat(4);
+            Console.WriteLine(kwadrat.Obwod());
+
+            Console.ReadKey();
+        }
+    }
+}
