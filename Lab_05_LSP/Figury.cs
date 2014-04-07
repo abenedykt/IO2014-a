8cdc03bebc1add6c3fc1439f32dc9588dba046b3
Marek Ormaniec <marokodanz@gmail.com>
2014-04-07 15:53:34 +0200

    LSP

---
+++ Lab_05_LSP/Figury.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab_05_LSP
+{
+    public class Kwadrat
+    {
+        protected int BokA { get; private set; }
+
+
+        public Kwadrat(int bokA)
+        {
+            BokA = bokA;
+        }
+
+        public virtual int Obwod()
+        {
+            return (4 * BokA);
+        }
+    }
+
+    public class Prostokat : Kwadrat
+    {
+        private int BokB { get; set; }
+
+        public Prostokat(int bokA, int bokB)
+            : base(bokA: bokA)
+        {
+            BokB = bokB;
+        }
+        public override int Obwod()
+        {
+            return (2 * BokA + 2 * BokB);
+        }
+    }
+}
---
+++ Lab_05_LSP/FiguryTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Lab_05_LSP
+{
+    public class FiguryTest
+    {
+        [Fact]
+        public void ObwodKwadratu()
+        {
+            Kwadrat o = new Prostokat(50,5);
+
+            Assert.Equal(110, o.Obwod());
+        }
+
+        [Fact]
+        public void ObwodKwadratu2()
+        {
+            var o = new Kwadrat(5);
+
+            Assert.Equal(20, o.Obwod());
+        }
+
+        [Fact]
+        public void ObwodProstokata()
+        {
+            var o = new Prostokat(10, 5);
+            Assert.Equal(30,o.Obwod());
+        }
+    }
+}
