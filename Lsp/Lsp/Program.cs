fd9b9ab7e07895e1ba4303cfb75f3a32da41df3a
Rafal Ficek <ficek.rafal@gmail.com>
2014-04-07 15:53:28 +0200

    Dodano Lsp

---
+++ Lsp/Lsp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lsp
+{
+    public class Prostokat
+    {
+        public readonly int A;
+        public readonly int B;
+
+        public Prostokat(int a, int b)
+        {
+            this.A = a;
+            this.B = b;
+        }
+
+        public int Obwod()
+        {
+            return 2 * A + 2 * B;
+        }
+
+        public int Pole()
+        {
+            return A * B;
+        }
+    }
+
+    public class Kwadrat : Prostokat
+    {
+        public Kwadrat(int a)
+            : base(a, a)
+        {
+        }
+    }
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var prostokat = new Kwadrat(10);
+
+            Console.WriteLine(prostokat.Obwod());
+
+            Console.ReadKey(true);
+        }
+    }
+}
