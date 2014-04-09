21d784cd57489de39ab847ee959e89b478a28d7e
pwlgbb <pgraczek@gmail.com>
2014-04-09 22:29:08 +0200

    lab7
    Signed-off-by: pwlgbb <pgraczek@gmail.com>

---
+++ Io_LSP/Io_LSP/Prostokat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Io_LSP
+{
+    public class Prostokat
+    {
+        private double p;
+        private double q;
+
+        public Prostokat(double p, double q)
+        {
+            if (p <= 0 || q <= 0)
+                throw new Exception("Boki muszą być większe od zera!");
+            this.p = p;
+            this.q = q;
+        }
+
+        public double Obwod()
+        {
+            return p * q;
+        }
+    }
+}
---
+++ Io_LSP/Io_LSP/Testy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Io_LSP
+{
+    [TestFixture]
+    public class Testy
+    {
+        [Test]
+        public void Prostokat2i3Test()
+        {
+            Prostokat prostokat = new Prostokat(2.0, 3.0);
+            Assert.AreEqual(6, prostokat.Obwod());
+        }
+
+        [Test]
+        public void ProstokatMinus2i3Test()
+        {
+            Assert.Throws<Exception>(() => new Prostokat(-2.0, 3.0));
+        }
+
+        [Test]
+        public void Prostokat2iMinus3Test()
+        {
+            Assert.Throws<Exception>(() => new Prostokat(2.0, -3.0));
+        }
+
+        [Test]
+        public void ProstokatMinus2iMinus3Test()
+        {
+            Assert.Throws<Exception>(() => new Prostokat(2.0, -3.0));
+        }
+
+        [Test]
+        public void Kwadrat4Test()
+        {
+            Kwadrat kwadrat = new Kwadrat(4);
+            Assert.AreEqual(16, kwadrat.Obwod());
+        }
+
+        [Test]
+        public void Kwadrat0Test()
+        {
+            Kwadrat kwadrat = new Kwadrat(4);
+            Assert.Throws<Exception>(() => new Kwadrat(0));
+        }
+
+        [Test]
+        public void KwadratMinus1Test()
+        {
+            Kwadrat kwadrat = new Kwadrat(4);
+            Assert.Throws<Exception>(() => new Kwadrat(-1));
+        }
+
+        [Test]
+        public void KwadratJakoProstokat4Test()
+        {
+            Prostokat kwadrat = new Kwadrat(4);
+            Assert.AreEqual(16, kwadrat.Obwod());
+        }
+    }
+}
