0bf26d549ab4384645a95281f1b842a7ad97c3a1
Krzysztof Bizon <krzys19914@o2.pl>
2014-05-04 23:11:08 +0200

    FizzBuzzWebApiKnockout

---
+++ Code/FizzBuzzWebApiKnockout/AngularTest/Scripts/app/main.js
@@ -0,0 +1,16 @@
+﻿function AppViewModel() {
+    var self = this;
+
+    self.input = ko.observable(0);
+    self.result = ko.observableArray();
+
+    self.change = function () {
+          if (self.input()) {
+        $.get("/api/fizzbuzz?count=" + self.input(), function (data) {
+            self.result(data);
+        });
+         }
+    };
+};
+
+ko.applyBindings(new AppViewModel());
