1683eb4cc91bd904fe94abb1a761d51c2abb3b74
Rafał Ples <pracowiciebezczynny@gmail.com>
2014-05-05 16:16:45 +0200

    zadanie 5, ndepend

---
+++ zadanie5/NDepend/AbstractInstability/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractInstability
+{
+    public interface Foo1
+    {
+        IEnumerable<string> Get { get; set; }
+    }
+
+
+}
