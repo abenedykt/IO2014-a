5f93b63e550593232a36de7a3af697f79cc542dc
Rafal Ficek <ficek.rafal@gmail.com>
2014-05-05 16:25:46 +0200

    NDepend

---
+++ NDepend/AbstractStable/ConcreteInstable/CI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractStable;
+
+namespace ConcreteInstable
+{
+    public class CI : AS1
+    {
+
+    }
+}
