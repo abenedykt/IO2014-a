4409945c930cc0c93fe15ea944b5d684c6f0728c
Marek Ormaniec <marokodanz@gmail.com>
2014-05-12 16:21:42 +0200

    Sprawdzanie GC w JS

---
+++ JS_GarbageCollector/JS_GarbageCollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JS_GarbageCollector.Startup))]
+namespace JS_GarbageCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
