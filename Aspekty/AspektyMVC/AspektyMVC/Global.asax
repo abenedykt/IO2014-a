6dd39d4e4daa7dab39cfb83fb5dc0335264960ad
Adam Kawik <akawik@gmail.com>
2014-05-19 17:03:11 +0200

    Dodanie aspektów

---
+++ Aspekty/AspektyMVC/AspektyMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspektyMVC.MvcApplication" Language="C#" %>
---
+++ Aspekty/AspektyMVC/AspektyMVC/Models/Open.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspektyMVC.Models
+{
+    public class Open : IOpenable
+    {
+        public string OpenSomethink()
+        {
+            return string.Format("Cos tam open!");
+        }
+    }
+}
