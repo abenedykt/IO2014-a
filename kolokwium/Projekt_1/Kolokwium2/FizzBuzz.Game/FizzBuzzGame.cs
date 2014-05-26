d11183d29908b460f2bdab53e3f1427b154cabf9
Marek Ormaniec <marokodanz@gmail.com>
2014-05-26 15:34:25 +0200

    Kolokwium 2 - Projekt 1 - TDD - Zrobione

---
+++ kolokwium/Projekt_1/Kolokwium2/FizzBuzz.Game/FizzBuzzGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz.Game
+{
+    public class ReturnType
+    {
+        public int ID { get; set; }
+        public string Value { get; set; }
+    }
+    public class FizzBuzzGame
+    {
+        public int N { get; set; }
+        public FizzBuzzGame()
+        {
+               
+        }
+        public FizzBuzzGame(string[] args)
+        {
+            N = Convert.ToInt32(args[0]);
+        }
+
+        public IEnumerable<ReturnType> ReturnCollection()
+        {
+            var tempCollection = new List<ReturnType>();
+            for (int i = 0; i < N; i++)
+            {
+                tempCollection.Add(new ReturnType{ ID = i+1, Value = Play(i+1).ToString()});
+            }
+            return tempCollection;
+        } 
+
+        public object Play(int p)
+        {
+            switch (p % 15)
+            {
+                case 0: return "FizzBuzz";
+                case 3: return "Fizz";
+                case 6: return "Fizz";
+                case 5: return "Buzz";
+                case 10: return "Buzz";
+                case 9: return "Fizz";
+                case 12: return "Fizz";
+                default: return p;
+            }
+         
+        }
+    }
+}
---
+++ kolokwium/Projekt_1/Kolokwium2/FizzBuzz.Test/FbTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FizzBuzz.Game;
+
+namespace FizzBuzz.Test
+{
+    public class FbTest
+    {
+        FizzBuzzGame game = new FizzBuzzGame();
+        public FbTest()
+        {
+            game = new FizzBuzzGame();
+        }
+        [Fact]
+        public void CzyDlaTrojkiJestFizz()
+        {
+            var p1 = game.Play(3);
+            Assert.Equal("Fizz", p1);
+        }
+
+        [Fact]
+        public void CzyDlaPodzielnejPrzezTrzyJestFizz()
+        {
+            var p1 = game.Play(6);
+            Assert.Equal("Fizz", p1);
+        }
+
+        [Fact]
+        public void CzyDlaPiatkiJestBuzz()
+        {
+            var p1 = game.Play(5);
+            Assert.Equal("Buzz", p1);
+        }
+        [Fact]
+        public void CzyDlaPietnastkiJestFizzBuzz()
+        {
+            var p1 = game.Play(15);
+            Assert.Equal("FizzBuzz", p1);
+        }
+
+    }
+}
