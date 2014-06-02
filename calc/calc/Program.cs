42ee64936827d0d5f6e99a3cf778c720dbe86cc0
Sebastian Czubienko <sebastian.czubienko@gmail.com>
2014-06-02 16:20:18 +0200

    da

---
+++ calc/calc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+
+        }
+    }
+}
---
+++ calc/testy/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace testy
+{
+    public class Class1
+    {
+        [Fact]
+        private void DwaPlusDwa()
+        {
+             using (var driver = new OpenQA.Selenium.Firefox.FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("http://webcalculator.apphb.com/Home/");
+
+                var AElement = driver.FindElementById("A");
+                var BElement = driver.FindElementById("B");
+
+                AElement.SendKeys("2");
+                BElement.SendKeys("2");
+
+                 Assert.Equal(2, 2);
+
+            }
+        }
+    }
+}
