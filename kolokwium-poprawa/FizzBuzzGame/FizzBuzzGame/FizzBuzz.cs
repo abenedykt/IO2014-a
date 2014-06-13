6bfe473b72a124c0ae8bbdaadcf3844bf6151473
Daredzik <d4r3d3wil@gmail.com>
2014-06-13 09:17:05 +0200

    Add FizzBuzz solution (works)

---
+++ kolokwium-poprawa/FizzBuzzGame/FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    public class FizzBuzz
+    {
+        public string Compute(int i)
+        {
+            if (i % 15 == 0)
+                return "FizzBuzz";
+            if (i % 3 == 0)
+                return "Fizz";
+            if (i % 5 == 0)
+                return "Buzz";
+
+            return i.ToString();
+        }
+
+        public IEnumerable<string> ComputeSequence()
+        {
+            var i = 1;
+            while (true)
+                yield return Compute(i++);
+        }
+
+    }
+}
---
+++ kolokwium-poprawa/FizzBuzzGame/FizzBuzzTests/FizzBuzzTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzGame;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    class FizzBuzzTest
+    {
+
+        [Fact]
+        public void ReturnOneOnOne()
+        {
+            var result = new FizzBuzz().Compute(1);
+
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void ReturnTwoOnTwo()
+        {
+            var result = new FizzBuzz().Compute(2);
+
+            Assert.Equal("2", result);
+        }
+
+        [Fact]
+        public void ReturnFizzOnThree()
+        {
+            var result = new FizzBuzz().Compute(3);
+
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void ReturnBuzzOnFive()
+        {
+            var result = new FizzBuzz().Compute(5);
+
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void ReturnFizzBuzzOnFifteen()
+        {
+            var result = new FizzBuzz().Compute(15);
+
+            Assert.Equal("FizzBuzz", result);
+        }
+
+        [Fact]
+        public void TestFirstFiveNumbers()
+        {
+            var result = new FizzBuzz().ComputeSequence().Take(5).ToList();
+
+            var expected = new List<string> { "1", "2", "Fizz", "4", "Buzz" };
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
